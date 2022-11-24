1161e7b4038c867fefd63a2c18219bf7bfa7a52f
noredcat <anastasiaredcat@gmail.com>
2022-11-24 23:12:34 +0700

    add properties

--- src/test/java/diplomTests/testsUI/TestBase.java
+++ src/test/java/diplomTests/testsUI/TestBase.java
@@ -17,35 +17,31 @@ public class TestBase {
     @BeforeAll
     static void configure() throws MalformedURLException {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        Configuration.browserCapabilities = capabilities;
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-
-
         if (System.getProperty("remote_url") != null) {
-
-            Configuration.browser = System.getProperty("browser_name");
-            Configuration.browserVersion = System.getProperty("browser_version");
-            Configuration.browserSize = System.getProperty("browser_size");
             Configuration.remote = System.getProperty("remote_url");
-        } else {
-            capabilities.setCapability("browserName", "chrome");
-            capabilities.setCapability("browserVersion", "100.0");
-            Configuration.browserSize = "1800x1200";
-
-
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
         }
+
+        Configuration.browser = System.getProperty("browser_name", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+
+        Configuration.browserCapabilities = capabilities;
     }
+
+
     @AfterEach
     void addAttachments() {
         Attach.screenshotsAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
-    }
+        if (System.getProperty("remote_url") != null) {
+            Attach.addVideo();
+        }
 
+    }
 }
