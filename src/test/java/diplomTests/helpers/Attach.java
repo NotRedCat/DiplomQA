e68a6f6d90e5899e228ed59c8fde20916b726e1d
noredcat <anastasiaredcat@gmail.com>
2022-11-25 16:17:04 +0700

    fix

--- src/test/java/diplomTests/helpers/Attach.java
+++ src/test/java/diplomTests/helpers/Attach.java
@@ -46,6 +46,7 @@ public class Attach {
 
     public static URL getVideoUrl() {
         String videoUrl = "https://selenoid.autotests.cloud/video/" + getSessionId() + ".mp4";
+
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
